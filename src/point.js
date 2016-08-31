0ed729ce306280e984cbaa0abe948ffa8d72066c
mperdikeas <mperdikeas@gmail.com>
2016-08-31 19:36:35 +0300

    @npm version 0.0.12 - conditionally require babel-polyfill

--- src/point.js
+++ src/point.js
@@ -6,7 +6,9 @@ require('source-map-support').install();
 // The rationale behind using this idiom is described in:
 //     http://stackoverflow.com/a/36628148/274677
 //
-require('babel-polyfill'); // this is important as Babel only transforms syntax (e.g. arrow functions)
+if (!global._babelPolyfill) // https://github.com/s-panferov/awesome-typescript-loader/issues/121
+    require('babel-polyfill');
+// The above is important as Babel only transforms syntax (e.g. arrow functions)
 // so you need this in order to support new globals or (in my experience) well-known Symbols, e.g. the following:
 //
 //     console.log(Object[Symbol.hasInstance]);
